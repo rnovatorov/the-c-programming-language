c7e4953e9f615416ff6c3342f024f762b48871de
Roman Novatorov <roman.novatorov@gmail.com>
2020-04-21 23:11:36 +0300

    tests: fix dynarray value comparison

--- tests/test_dynarray.c
+++ tests/test_dynarray.c
@@ -1,6 +1,7 @@
 #include <assert.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 #include "../lib/dynarray.h"
 
@@ -8,28 +9,26 @@ int main() {
     struct dynarray *d = dynarray_alloc(0);
     assert(dynarray_len(d) == 0);
 
-    char *const foo = "foo";
-    assert(dynarray_append(d, foo));
+    assert(dynarray_append(d, "foo"));
     assert(dynarray_len(d) == 1);
-    assert(dynarray_get(d, 0) == foo);
+    assert(!strcmp(dynarray_get(d, 0), "foo"));
 
-    char *const bar = "bar";
-    assert(dynarray_append(d, bar));
+    assert(dynarray_append(d, "bar"));
     assert(dynarray_len(d) == 2);
-    assert(dynarray_get(d, 0) == foo);
-    assert(dynarray_get(d, 1) == bar);
+    assert(!strcmp(dynarray_get(d, 0), "foo"));
+    assert(!strcmp(dynarray_get(d, 1), "bar"));
 
-    dynarray_set(d, 0, bar);
-    dynarray_set(d, 1, foo);
+    dynarray_set(d, 0, "bar");
+    dynarray_set(d, 1, "foo");
     assert(dynarray_len(d) == 2);
-    assert(dynarray_get(d, 0) == bar);
-    assert(dynarray_get(d, 1) == foo);
+    assert(!strcmp(dynarray_get(d, 0), "bar"));
+    assert(!strcmp(dynarray_get(d, 1), "foo"));
 
-    assert(dynarray_pop(d) == foo);
+    assert(!strcmp(dynarray_pop(d), "foo"));
     assert(dynarray_len(d) == 1);
-    assert(dynarray_get(d, 0) == bar);
+    assert(!strcmp(dynarray_get(d, 0), "bar"));
 
-    assert(dynarray_pop(d) == bar);
+    assert(!strcmp(dynarray_pop(d), "bar"));
     assert(dynarray_len(d) == 0);
 
     dynarray_free(d);
